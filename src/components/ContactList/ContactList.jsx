e4b3d7acaaec7d7fac68e3ef8e43fa7539f8d1a0
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-05-04 16:32:49 +0300

    4

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,25 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
 import s from './ContactList.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsSlice';
 
 const ContactList = () => {
+    const dispatch = useDispatch();
     const contacts = useSelector(state => state.contacts.items);
-    const filter = useSelector(state => state.filters.name.toLowerCase());
+    const filter = useSelector(state => state.filters.name);
 
     const filteredContacts = contacts.filter(contact =>
-        contact.name.toLowerCase().includes(filter)
+        contact.name.toLowerCase().includes(filter.toLowerCase())
     );
 
     return (
         <ul className={s.list}>
             {filteredContacts.map(({ id, name, number }) => (
-                <Contact key={id} id={id} name={name} number={number} />
+                <li key={id}>
+                    {name}: {number}
+                    <button onClick={() => dispatch(deleteContact(id))}>Delete</button>
+                </li>
             ))}
         </ul>
     );
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,14 +1,23 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { changeFilter } from '../../redux/filtersSlice';
 import s from './SearchBox.module.css';
 
-const SearchBox = ({ value, onChange }) => {
+const SearchBox = () => {
+    const dispatch = useDispatch();
+    const filter = useSelector(state => state.filters.name);
+
+    const handleChange = (value) => {
+        dispatch(changeFilter(value));
+    };
+
     return (
         <div className={s.box}>
             <h2 className={s.text}>Find contacts by name</h2>
             <input
                 className={s.input}
                 type="text"
-                value={value}
-                onChange={(e) => onChange(e.target.value)}
+                value={filter}
+                onChange={(e) => handleChange(e.target.value)}
             />
         </div>
     );
